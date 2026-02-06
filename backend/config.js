9507fddb48a6a36a15065235f70ce15e9496c116
Aniket Maurya <aniketmaurya.dev@gmail.com>
2026-02-07 04:54:10 +0530

    Update mongoDBURL to use MONGODB_URI with fallback

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,4 @@
 export const PORT = process.env.PORT || 5000;
 
-export const mongoDBURL = process.env.MONGODB_URL;
-
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
+export const mongoDBURL =
+  process.env.MONGODB_URI || 'mongodb://mongodb:27017/mern_db';
